fa36ed6313c233a5a38e121e68b2f009e303e46d
koryun23 <koryun.grigoryan.5@gmail.com>
2021-01-25 16:49:32 +0400

    creating the dictionary using mysql database

--- exercise.py
+++ exercise.py
@@ -6,3 +6,15 @@ con = mysql.connector.connect(
     host = "108.167.140.122",
     database = "ardit700_pm1database"
 )
+
+cursor = con.cursor()
+
+word = input("Enter a word: ")
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s' " % word)
+results = cursor.fetchall()
+
+if results:
+    for i in range(len(results)):
+        print(i+1,':',results[i][1])
+else:
+    print("No words found")
